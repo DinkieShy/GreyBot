9d2db69532918447fb1238ba1d5d10c5a92da1ae
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2023-06-03 08:23:58 +0100

    Update pronouns.js

--- commands/pronouns.js
+++ commands/pronouns.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,42 +12,28 @@ module.exports = {
 
 	async getPermissions(guild){
 	  return {
-        id: guild.roles.cache.find(item => item.name == "Members").id,
+        id: guild.roles.cache.find(item => item.name == "Member").id,
         type: "ROLE",
         permission: true
       };
 	},
 
 	async execute(interaction){
-		const roleToAssign = interaction.options.getString("pronouns");
-		if(roleToAssign == ""){
-			interaction.reply({content: "You forgot to write your pronouns!", ephemeral: true});
+		const roleToAssign = interaction.options.getString("pronouns").trim().toLowerCase();
+		const roles = await interaction.guild.roles.fetch();
+
+		const memberCurrentRoles = interaction.member.roles.cache.filter(role => role.name.slice(0,10) == "pronouns: ")
+		await interaction.member.roles.remove(memberCurrentRoles, `Removing old pronoun roles from ${interaction.user.username}`); 
+
+		const existingRole = roles.find(role => role.name.slice(10) == roleToAssign);
+		if(existingRole != undefined){
+			await interaction.member.roles.add(existingRole).then(async newMember => {
+				await interaction.reply("Enjoy your new pronoun role!");
+			}).catch(async error => {
+				await interaction.followUp("There was an error! Please poke Grey ;-; (pronouns.js:30)");
+			});
 		}
-		const roles = interaction.guild.roles.cache;
-		const availablePronounRoles = roles.filter(item => item.name.includes("pronouns: "));
-		var roleFound = false;
-
-		availablePronounRoles.each(async role => {
-			if(role.name.slice(10) == roleToAssign){
-				await interaction.member.roles.add(role).then(async newMember => {
-					await interaction.reply("Enjoy your new pronoun role!");
-				}).catch(async error => {
-					console.log(error);
-					await interaction.followUp("There was an error! Go poke Dinkie please ;-;");
-				});
-				roleFound = true;
-			}
-			else{
-				if(role.members.get(interaction.user.id) != undefined){
-					await interaction.member.roles.remove(role);
-				}
-				if(role.members.size == 0){
-					await role.delete("Not being used at the moment!");
-				}
-			}
-		});
-
-		if(roleFound == false){
+		else{
 			interaction.guild.roles.create({
 				name: "pronouns: " + roleToAssign,
 				reason: `Adding pronoun role for ${interaction.user.username}`
@@ -56,8 +42,47 @@ module.exports = {
 				await interaction.reply("Enjoy your new pronoun role!");
 			}).catch(async error => {
 				console.log(error);
-				await interaction.followUp("There was an error! Go poke Dinkie please ;-;");
+				await interaction.followUp("There was an error! Please poke Grey ;-; (pronouns.js:42)");
 			});
 		}
+
+		interaction.guild.roles.filter(role => role.name.slice(0,10) == "pronouns: ").each(async role => {
+			if(role.members.size == 0){
+				await role.delete("Deleting unused pronoun role");
+			}
+		});
+
+		// availablePronounRoles.each(async role => {
+		// 	if(role.name.slice(10) == roleToAssign){
+		// 		await interaction.member.roles.add(role).then(async newMember => {
+		// 			await interaction.reply("Enjoy your new pronoun role!");
+		// 		}).catch(async error => {
+		// 			console.log(error);
+		// 			await interaction.followUp("There was an error! Go poke Grey please ;-; (pronouns.js:33)");
+		// 		});
+		// 		roleFound = true;
+		// 	}
+		// 	else{
+		// 		if(role.members.get(interaction.user.id) != undefined){
+		// 			await interaction.member.roles.remove(role);
+		// 		}
+		// 		if(role.members.size == 0){
+		// 			await role.delete("Not being used at the moment!");
+		// 		}
+		// 	}
+		// });
+
+		// if(roleFound == false){
+		// 	interaction.guild.roles.create({
+		// 		name: "pronouns: " + roleToAssign,
+		// 		reason: `Adding pronoun role for ${interaction.user.username}`
+		// 	}).then(async newRole => {
+		// 		interaction.member.roles.add(newRole);
+		// 		await interaction.reply("Enjoy your new pronoun role!");
+		// 	}).catch(async error => {
+		// 		console.log(error);
+		// 		await interaction.followUp("There was an error! Go poke Grey please ;-;");
+		// 	});
+		// }
 	}
 }
